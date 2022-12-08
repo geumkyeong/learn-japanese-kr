f03d77d5b9f1a56865837cf3e1839722cf71694f
geumkyeong <ansrmarud@gmail.com>
2022-12-09 00:40:50 +0900

    fix cannot access static files

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static("build"));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: path.join(__dirname, "build") }, (err) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'), (err) => {
     if (err) {
       res.status("500").send(err);
     }
