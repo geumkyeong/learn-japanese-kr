c306b45d6f4c07e586eeeee339de054565ef5de4
geumkyeong <ansrmarud@gmail.com>
2022-12-09 14:47:46 +0900

    Connecting to MongoDB

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
+const Dictionary = require("./models/dictionary");
+const config = require("./config/keys");
 const path = require("path");
+const cors = require("cors");
 
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
+
+mongoose
+  .connect(config.mongoURI)
+  .then(() => {
+    console.log("MongoDB is Connected!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'), (err) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"), (err) => {
     if (err) {
       res.status("500").send(err);
     }
---
+++ models/dictionary.js
@@ -0,0 +1,17 @@
+const dictSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      maxlength: 50,
+    },
+    description: {
+      type: String,
+      maxlength: 100,
+    },
+  },
+  { collection: "dictionaries" }
+);
+
+const Dictionary = mongoose.model("Dictionary", dictSchema);
+
+module.exports = Dictionary;
